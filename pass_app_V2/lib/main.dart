daa3ab76164da874d60e4ee1bb271a833eb1a78c
MarkQura <69739495+MarkQura@users.noreply.github.com>
2020-11-25 20:49:42 +0000

    Login Screen

--- pass_app_V2/lib/main.dart
+++ pass_app_V2/lib/main.dart
@@ -1,48 +1,20 @@
 import 'package:flutter/material.dart';
+import 'Screens/MainScreen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+          canvasColor: Colors.grey[900],
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: MyHomePage(),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +22,210 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _obscureText = true;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void update(callback) {
+    this.setState(() {
+      callback();
     });
+  }
+
+  void unlock() {
+    _obscureText = !_obscureText;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return GestureDetector(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 90),
+                child: Text(
+                  'Iniciar Sessão',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 50, right: 20, left: 20),
+                child: TextField(
+                  cursorColor: Colors.blueGrey,
+                  style: TextStyle(color: Colors.cyan[200]),
+                  decoration: InputDecoration(
+                    labelText: 'Nome de Utilizador',
+                    labelStyle: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.cyan[200]),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 30,
+                  right: 20,
+                  left: 20,
+                ),
+                child: TextField(
+                  cursorColor: Colors.blueGrey,
+                  style: TextStyle(color: Colors.cyan[200]),
+                  decoration: InputDecoration(
+                    labelText: 'Palavra Passe',
+                    labelStyle: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: (_obscureText
+                          ? Icon(Icons.lock)
+                          : Icon(Icons.lock_open)),
+                      color: Colors.blueGrey,
+                      onPressed: () => update(unlock),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey),
+                    ),
+                  ),
+                  obscureText: _obscureText,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(10, 25, 10, 20),
+                        child: Divider(
+                          thickness: 5,
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "OU",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(10, 25, 10, 20),
+                        child: Divider(
+                          thickness: 5,
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 10,
+                  right: 10,
+                  left: 10,
+                ),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  splashColor: Colors.grey,
+                  color: Colors.grey[700],
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                      bottom: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/google_logo.png'),
+                          height: 35,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Iniciar sessão com Google',
+                            style: TextStyle(
+                              color: Colors.grey[900],
+                              fontSize: 23,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainScreen()),
+                    );
+                  },
+                ),
+              ),
+              Container(
+                color: Colors.transparent,
+                padding: EdgeInsets.only(top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 40,
+                        padding: EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          splashColor: Colors.grey[800],
+                          color: Colors.black,
+                          child: Text(
+                            'Registar-se',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MainScreen()),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
     );
   }
 }
